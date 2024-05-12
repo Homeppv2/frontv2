93be01026a2b44a40bdbc18d940dec08f382a49c
lim5max <lionmaks@list.ru>
2024-05-12 18:01:05 +0300

    change logo, text, login form

--- app/(dashboard)/dashboard/employee/page.tsx
+++ app/(dashboard)/dashboard/employee/page.tsx
@@ -23,14 +23,16 @@ export default async function page({ searchParams }: paramsProps) {
   const country = searchParams.search || null;
   const offset = (page - 1) * pageLimit;
 
-  const res = await fetch(
-    `https://api.slingacademy.com/v1/sample-data/users?offset=${offset}&limit=${pageLimit}` +
-      (country ? `&search=${country}` : ""),
-  );
-  const employeeRes = await res.json();
-  const totalUsers = employeeRes.total_users; //1000
+  // const res = await fetch(
+  //   `https://api.slingacademy.com/v1/sample-data/users?offset=${offset}&limit=${pageLimit}` +
+  //     (country ? `&search=${country}` : ""),
+  // );
+  // const employeeRes = await res.json();
+  // const totalUsers = employeeRes.total_users || 10; //1000
+  const totalUsers =  10; //1000
   const pageCount = Math.ceil(totalUsers / pageLimit);
-  const employee: Employee[] = employeeRes.users;
+  // const employee: Employee[] = employeeRes.users;
+  const employee: Employee[] = [];
   return (
     <>
       <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
